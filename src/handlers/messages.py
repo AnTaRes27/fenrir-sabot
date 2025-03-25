286dccdce9fac2cdb37bc98c72f9d63423d56e8e
Antares Husky <husky@antares.dog>
2025-03-24 21:49:05 -0500

    chore: black lint

--- src/handlers/messages.py
+++ src/handlers/messages.py
@@ -20,6 +20,7 @@ logger = logging.getLogger(__name__)
 db_handler = None
 config = None
 
+
 def init(app: Any, config_obj: Config, _db_handler: DbHandler):
     global config, db_handler
 
@@ -28,11 +29,19 @@ def init(app: Any, config_obj: Config, _db_handler: DbHandler):
 
     register_message_handlers(app)
 
+
 def register_message_handlers(app):
-    app.add_handler(MessageHandler(filters.Dice.SLOT_MACHINE & ~filters.FORWARDED, slot_machine_handler))
+    app.add_handler(
+        MessageHandler(
+            filters.Dice.SLOT_MACHINE & ~filters.FORWARDED, slot_machine_handler
+        )
+    )
     app.add_handler(MessageHandler(None, debugging))
 
-async def slot_machine_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+async def slot_machine_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
     id = update.message.from_user.id
     name = update.message.from_user.full_name
     username = update.message.from_user.username
@@ -41,7 +50,13 @@ async def slot_machine_handler(update: Update, context: ContextTypes.DEFAULT_TYP
 
     # Process and get updated balance
     balance = db_handler.process_slot_machine(
-        id, name, username, value, constants.DiceEmoji.SLOT_MACHINE, config.paytable, bet_cents
+        id,
+        name,
+        username,
+        value,
+        constants.DiceEmoji.SLOT_MACHINE,
+        config.paytable,
+        bet_cents,
     )
 
     combo_name = db_handler.get_combo_name(value)
@@ -50,11 +65,16 @@ async def slot_machine_handler(update: Update, context: ContextTypes.DEFAULT_TYP
         f"""{name}({id}) - {combo_name} - Total Games: {sum(data["tally"])} - Balance: {parse_dollar_amount(balance)}"""
     )
 
-    if value in (db_handler.TRIPLE_LEMON, db_handler.TRIPLE_GRAPE, db_handler.TRIPLE_BAR):
+    if value in (
+        db_handler.TRIPLE_LEMON,
+        db_handler.TRIPLE_GRAPE,
+        db_handler.TRIPLE_BAR,
+    ):
         await update.message.set_reaction("✨")
     elif value == db_handler.TRIPLE_SEVEN:
         await update.message.set_reaction("🔥")
         await update.message.reply_text("Ahoy!")
+
 
 async def debugging(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """logs user message for debugging"""
